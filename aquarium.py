62d5681512b7f93b0f54950a4c6562eeeb6b2580
emuzzi19 <90274073+emuzzi19@users.noreply.github.com>
2023-11-02 10:10:58 -0400

    Update aquarium.py - Added Constants

--- aquarium.py
+++ aquarium.py
@@ -7,3 +7,23 @@
 #
 #
 #=====================================================================================================================================================================================================================
+
+
+# Constants
+Xmin = 0
+Xmax = (24*Math.pi)
+Ymin = -(6*Math.pi)
+Ymax = (5* Math.pi)
+Zmin = (2* Math.pi)
+Zmax = (5* Math.pow(10,-6))
+Wishbone1 = (2 * Math.pow(10,-8))
+Wishbone2 = (3 * Math.pow(10,-6))
+V = 3628800
+
+
+# Chemical Consentrations
+
+C1 = 0 # NH4 (Ammonia)
+C2 = 0 # O2 (Oxygen)
+C3 = 0 # NO2 (NitrITE)
+C4 = 0 # NO3 (NitrATE)
