c96624e4d19c579c009e2dbe0e910681423722d8
emuzzi19 <emuzzi19@gmail.com>
2023-11-09 06:57:17 -0800

    Created Fish object

--- Fish/fish.py
+++ Fish/fish.py
@@ -2,8 +2,22 @@
 #                                                                     ~ Continuous-Time Simulation Of Chemical Concentration in Aquariums ~
 #                                                                      File: fish.py
 #                                                                      Authors: 
-#                                                                      Description: 'Fish' interface, used to implement fish objects to interact with the system  
+#                                                                      Description: 'Fish' object, used to implement fish objects to interact with the system  
 #
 #
 #
 #=====================================================================================================================================================================================================================
+
+class Fish:
+     def __init__(self, EatChance, PoopChance, DeathChance,ServingSize,PoopAmount,Living):
+        self.EatChance = EatChance
+        self.PoopChance = PoopChance
+        self.DeathChance = DeathChance
+        self.ServingSize = ServingSize
+        self.PoopAmount = PoopAmount
+        self.Living = Living
+        
+     
+        
+    
+        
