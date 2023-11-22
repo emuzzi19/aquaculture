8660551d3b58a7dfd54dd5466fe4889a5018e0e1
emuzzi19 <emuzzi19@gmail.com>
2023-11-21 21:01:42 -0800

    - removed poop amount - added string status to instance data - added eat method - added poop method - added pee method - started working on grow function (Further validation needed) - reworked action function to update staus and return both status and amount - made the random generated probablities instance data - added logic for when nothing happens

--- Fish/fish.py
+++ Fish/fish.py
@@ -8,16 +8,62 @@
 #
 #=====================================================================================================================================================================================================================
 
-class Fish:
-     def __init__(self, EatChance, PoopChance, DeathChance,ServingSize,PoopAmount,Living):
-        self.EatChance = EatChance
-        self.PoopChance = PoopChance
-        self.DeathChance = DeathChance
-        self.ServingSize = ServingSize
-        self.PoopAmount = PoopAmount
-        self.Living = Living
-        
-     
-        
+import random 
+
+class fish:
+
+    def __init__(self, name, eatPercent, poopPercent,peePercent, deathPercent, servingSize, weight):
+        self.name = name
+        self.eatPercent = eatPercent
+        self.poopPercent = poopPercent
+        self.peePercent = peePercent
+        self.deathPercent = deathPercent
+        self.servingSize = servingSize
+        self.weight = weight
+        self.living = True
+        self.status = 'Nothing'
     
+    #these still need work, specifically in calculating the chemical changes
+    
+    def Eat(self):
+        self.status = 'Eating'
+        result = (self.weight*self.servingSize)
+        return result
         
+    def Poop(self):
+        self.status = 'Pooping'
+        result = (self.weight/3)
+        return result
+        
+    def Pee(self,t):
+        self.status = 'Peeing'
+        hours = t/3600
+        result = (pow((0.0014*self.weight),2)-0.4384*self.weight+43.303)
+        result = result*hours
+        return result
+    
+
+    #def Grow(self,t):
+        #days = t/86400
+       # while self.weight <= 450:
+            #self.weight = pow((0.4244*days),2)+(83.729*days) 
+            
+
+
+    def action(self,t):
+        fishActionProb = random.random()
+        if fishActionProb >= 0.0 and fishActionProb <= self.eatPercent:      #fish eats if the random number is in this range 
+            result = self.Eat() 
+        if fishActionProb > self.eatPercent and fishActionProb <= self.poopPercent: #fish poops if the random number is in this range   
+            result = self.Poop()
+        if fishActionProb > self.poopPercent and fishActionProb <= self.peePercent: #fish pees if the random number is in this range
+            result = self.Pee(t) 
+        else:                                                          #if none of these if statements are fulfilled, the fish is doing no actions
+            result = 0
+            self.status = "Nothing"
+        return result,self.status
+            
+       
+
+
+   
